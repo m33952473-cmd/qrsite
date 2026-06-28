e1566b6fae2769025e6a37a1ec30ee3636fd555d
m33952473-cmd <m33952473@gmail.com>
2026-06-29 00:11:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ let currentReportNo = "";
 function uploadFile() {
     const file = document.getElementById("fileInput").files[0];
     const reportNo = document.getElementById("reportNo").value;
-    const status = document.getElementById("statusMsg");
 
     if(!reportNo){
         alert("Enter Report No");
@@ -11,33 +10,27 @@ function uploadFile() {
     }
 
     currentReportNo = reportNo;
-    status.innerHTML = "Uploading document... 0%";
 
     const formData = new FormData();
     formData.append("file", file);
     formData.append("upload_preset", "hertzupload");
 
-    // ⭐ file ka naam report no. banega
-    formData.append("public_id", reportNo);
-
     const xhr = new XMLHttpRequest();
     xhr.open("POST", "https://api.cloudinary.com/v1_1/dweefwzka/upload");
 
-    xhr.upload.onprogress = function (e) {
-        if (e.lengthComputable) {
-            let percent = Math.round((e.loaded / e.total) * 100);
-            status.innerHTML = "Uploading document... " + percent + "%";
-        }
-    };
-
     xhr.onload = function () {
-        status.innerHTML = "Generating QR Code...";
+        const data = JSON.parse(xhr.responseText);
+        const fileUrl = data.secure_url;
+
+        // sirf file name nikaalna
+        const fileName = fileUrl.split("/").pop();
 
         const finalUrl =
-        "https://hertzinspn.co.in/documents/" + reportNo;
+        "https://hertzinspn.co.in/documents/" + fileName;
 
-        QRCode.toCanvas(document.getElementById('qrcode'), finalUrl, function () {
-            status.innerHTML = "✅ Upload Complete & QR Ready";
+        const canvas = document.getElementById('qrcode');
+
+        QRCode.toCanvas(canvas, finalUrl, function () {
             document.getElementById("downloadBtn").style.display = "inline-block";
         });
     };
